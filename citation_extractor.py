ef234221f6b33c736d3c970b151f950339e88141
Will Oursler <woursler@gmail.com>
2022-07-24 21:09:17 -0700

    Add citations blocks.

--- citation_extractor.py
+++ citation_extractor.py
@@ -1,91 +1,65 @@
-# TODO(woursler): Extract URLS directly from libsyn_details.csv
+import pandas as pd
+import requests
+import time
+import pandoc
+import wikitextparser
 
-'''
-LINKS = []
+from bs4 import BeautifulSoup
 
-	ep_record['mediawiki_description'] = pandoc.write(
-		pandoc.read(ep_record['details_html'], format="html-native_divs-native_spans"),
-		format="mediawiki"
-	)
+details_table = pd.read_csv('libsyn_details.csv', encoding ='latin1')
 
-	parsed_description = wikitextparser.parse(ep_record['mediawiki_description'])
 
-	if len(parsed_description.external_links) > 0:
-		print(ep_record['title'])
-		print(parsed_description.external_links)
-		LINKS.extend(parsed_description.external_links)
+URLS = []
+for ep_record in details_table.to_dict(orient='records'):
 
-for link in LINKS:
-	print(link.url)
+    ep_record['mediawiki_description'] = pandoc.write(
+        pandoc.read(ep_record['details_html'], format="html-native_divs-native_spans"),
+        format="mediawiki"
+    )
 
-'''
+    parsed_description = wikitextparser.parse(ep_record['mediawiki_description'])
 
-URLS = [
-    "https://knowledgefight.com/research/2021/10/10/episode-604-october-5-2021",
-    "https://knowledgefight.com/research/2021/10/12/episode-605-june-17-2003",
-    "https://knowledgefight.com/research/2021/10/14/episode-606-june-18-19-2003",
-    "https://knowledgefight.com/research/2021/10/24/episode-609-october-20-2021",
-    "https://knowledgefight.com/research/2021/10/31/episode-611-oct-29-2021",
-    "https://knowledgefight.com/research/2021/10/7/episode-603-october-3-4-2021",
-    "https://knowledgefight.com/research/2021/11/11/episode-616-november-9-2021",
-    "https://knowledgefight.com/research/2021/11/14/episode-617-june-27-2003",
-    "https://knowledgefight.com/research/2021/11/16/episode-618-november-15-2021",
-    "https://knowledgefight.com/research/2021/11/21/episode-619-november-16-2021",
-    "https://knowledgefight.com/research/2021/11/25/episode-620-november-23-2021",
-    "https://knowledgefight.com/research/2021/11/28/episode-621-reset-wars-episode-1",
-    "https://knowledgefight.com/research/2021/11/3/episode-612-october-31-2021",
-    "https://knowledgefight.com/research/2021/11/30/episode-622-june-30-july-1-2003",
-    "https://knowledgefight.com/research/2021/11/4/episode-613-june-24-25-2003",
-    "https://knowledgefight.com/research/2021/11/7/episode-614-the-purge-of-gates",
-    "https://knowledgefight.com/research/2021/12/12/episode-626-december-9-2021",
-    "https://knowledgefight.com/research/2021/12/16/episode-628-july-10-2003",
-    "https://knowledgefight.com/research/2021/12/2/episode-623-july-2-3-2003",
-    "https://knowledgefight.com/research/2021/12/29/episode-631-december-20-2021",
-    "https://knowledgefight.com/research/2021/12/7/episode-624-november-30-2021",
-    "https://knowledgefight.com/research/2021/12/9/episode-625-december-5-2021",
-    "https://knowledgefight.com/research/2021/7/11/episode-576-july-9-2021",
-    "https://knowledgefight.com/research/2021/7/15/episode-577-july-13-2021",
-    "https://knowledgefight.com/research/2021/7/18/episode-578-a-little-side-track",
-    "https://knowledgefight.com/research/2021/7/20/episode-579-may-30-2003",
-    "https://knowledgefight.com/research/2021/7/22/episode-580-july-20-21-2021",
-    "https://knowledgefight.com/research/2021/7/6/episode-574-may-28-29-2003",
-    "https://knowledgefight.com/research/2021/7/8/episode-575-july-4-2021",
-    "https://knowledgefight.com/research/2021/8/10/episode-585-august-8-2021",
-    "https://knowledgefight.com/research/2021/8/15/episode-586-august-11-12-2021",
-    "https://knowledgefight.com/research/2021/8/19/episode-588-august-15-2021",
-    "https://knowledgefight.com/research/2021/8/22/episode-589-august-18-20-2021",
-    "https://knowledgefight.com/research/2021/8/26/episode-590-august-24-2021",
-    "https://knowledgefight.com/research/2021/8/29/episode-591-june-11-2003",
-    "https://knowledgefight.com/research/2021/8/3/episode-583-august-1-2021",
-    "https://knowledgefight.com/research/2021/8/8/episode-584-august-5-2021",
-    "https://knowledgefight.com/research/2021/9/16/episode-597-september-14-2021",
-    "https://knowledgefight.com/research/2021/9/19/episode-598-september-17-2021",
-    "https://knowledgefight.com/research/2021/9/23/episode-599-june-13-2003",
-    "https://knowledgefight.com/research/2021/9/26/episode-600-september-22-2021",
-    "https://knowledgefight.com/research/2021/9/28/episode-601-june-16-2003",
-    "https://knowledgefight.com/research/2021/9/9/episode-594-september-7-2021",
-    "https://knowledgefight.com/research/2022/1/13/episode-636-april-27-2009",
-    "https://knowledgefight.com/research/2022/1/16/episode-637-january-13-2022",
-    "https://knowledgefight.com/research/2022/1/18/episode-638-see-you-at-the-cross-rhodes",
-    "https://knowledgefight.com/research/2022/1/2/episode-633-december-30-2021",
-    "https://knowledgefight.com/research/2022/1/20/episode-639-january-17-2022",
-    "https://knowledgefight.com/research/2022/1/23/episode-640-january-22-2022",
-    "https://knowledgefight.com/research/2022/1/28/episode-642-january-24-2022",
-    "https://knowledgefight.com/research/2022/1/30/episode-643-january-28-2022",
-    "https://knowledgefight.com/research/2022/1/6/episode-634-july-11-2003",
-    "https://knowledgefight.com/research/2022/1/9/episode-635-january-6-7-2022",
-    "https://knowledgefight.com/research/2022/2/11/episode-647-february-8-2022",
-    "https://knowledgefight.com/research/2022/2/13/episode-648-february-11-2022",
-    "https://knowledgefight.com/research/2022/2/15/episode-649-february-14-2022",
-    "https://knowledgefight.com/research/2022/2/17/episode-650-july-17-2003",
-    "https://knowledgefight.com/research/2022/2/20/episode-651-february-18-2022",
-    "https://knowledgefight.com/research/2022/2/25/episode-652-february-21-2022",
-    "https://knowledgefight.com/research/2022/2/27/episode-653-february-25-2022",
-    "https://knowledgefight.com/research/2022/2/3/episode-644-february-2-2022",
-    "https://knowledgefight.com/research/2022/2/9/episode-646-february-6-2022",
-    "https://knowledgefight.com/research/2022/3/1/episode-654-february-27-2022",
-    "https://knowledgefight.com/research/2022/3/3/episode-655-july-18-2003",
+    if len(parsed_description.external_links) > 0:
+        for link in parsed_description.external_links:
+            link_url = link.url
+            if 'knowledgefight.com/research/' in link_url:
+                URLS.append(link_url)
+
+print("Located", len(URLS), "citation urls.")
+
+header = [
+    "citations_url",
+    "citations_html",
+    "citations_mediawiki",
 ]
+rows = []
 
-for url in URLS:
-	print(url)
+for citations_url in URLS:
+    citations_page = None
+    while citations_page is None or citations_page.status_code != 200:
+        # SquareSpace is super senstive to too many requests.
+        time.sleep(2)
+        print('.')
+        citations_page = requests.get(citations_url)
+    citations_soup = BeautifulSoup(citations_page.text, 'html.parser')
+    citations_html = citations_soup.find("div", class_="entry-content")
+
+
+    citations_mediawiki = pandoc.write(
+        pandoc.read(citations_html, format="html-native_divs-native_spans"),
+        format="mediawiki"
+    )
+
+    print("Finished", citations_url)
+    # print(citations_mediawiki)
+
+    rows.append([
+        citations_url,
+        citations_html,
+        citations_mediawiki,
+    ])
+
+df = pd.DataFrame(rows, columns=header)
+
+with open("citations.csv", "w") as csv_file:
+    csv_file.write(df.to_csv(index=False, line_terminator='\n'))
--- stamp_template.py
+++ stamp_template.py
@@ -11,119 +11,124 @@ import io
 
 merged_df = pd.read_csv('merged.csv')
 
+citations_df = pd.read_csv('citations.csv', encoding ='latin1')
+
 with open('episode.wiki.template') as episode_template_f:
-	template = Template(episode_template_f.read())
+    template = Template(episode_template_f.read())
 
 
 def cleans(s):
-	if isinstance(s, float) and math.isnan(s):
-		return None
-	return s.strip()
+    if isinstance(s, float) and math.isnan(s):
+        return None
+    return s.strip()
 
 def cleantitle(s):
-	if isinstance(s, float) and math.isnan(s):
-		return None
-	s = s.strip()
-	if s[0] =='#':
-		s = s[1:].strip()
-	return s
+    if isinstance(s, float) and math.isnan(s):
+        return None
+    s = s.strip()
+    if s[0] =='#':
+        s = s[1:].strip()
+    return s
 
 def agressive_splits(s):
-	if isinstance(s, float) and math.isnan(s):
-		return []
+    if isinstance(s, float) and math.isnan(s):
+        return []
 
-	l1 = re.split(',|;', s)
-	l2 = [x.strip() for x in l1]
-	return [x for x in l2 if len(x) > 0]
+    l1 = re.split(',|;', s)
+    l2 = [x.strip() for x in l1]
+    return [x for x in l2 if len(x) > 0]
 
 def splits(s):
-	if isinstance(s, float) and math.isnan(s):
-		return []
+    if isinstance(s, float) and math.isnan(s):
+        return []
 
-	l1 = s.split(";")
-	l2 = [x.strip() for x in l1]
-	return [x for x in l2 if len(x) > 0]
+    l1 = s.split(";")
+    l2 = [x.strip() for x in l1]
+    return [x for x in l2 if len(x) > 0]
 
 TIMEZONE = 'US/Eastern'
 DATE_FORMAT = "%B %#d, %Y"
 
 def process_ep_record(ep_record):
-	ep_record['title'] = cleantitle(ep_record['title'])
-	ep_record['prev_title'] = cleantitle(ep_record['prev_title'])
-	ep_record['next_title'] = cleantitle(ep_record['next_title'])
+    ep_record['title'] = cleantitle(ep_record['title'])
+    ep_record['prev_title'] = cleantitle(ep_record['prev_title'])
+    ep_record['next_title'] = cleantitle(ep_record['next_title'])
 
-	ep_record['clean_title'] = ep_record['title'].split(':')[-1].strip()
+    ep_record['clean_title'] = ep_record['title'].split(':')[-1].strip()
 
-	ep_record['ooc_drop'] = cleans(ep_record['ooc_drop'])
-	ep_record['beverage'] = cleans(ep_record['beverage'])
-	ep_record['people'] = agressive_splits(ep_record['people'])
-	ep_record['sources'] = agressive_splits(ep_record['sources'])
-	ep_record['themes'] = splits(ep_record['themes'])
-	ep_record['notable_bits'] = splits(ep_record['notable_bits'])
-	ep_record['mediawiki_description'] = pandoc.write(
-		pandoc.read(ep_record['details_html'], format="html-native_divs-native_spans"),
-		format="mediawiki"
-	)
+    ep_record['ooc_drop'] = cleans(ep_record['ooc_drop'])
+    ep_record['beverage'] = cleans(ep_record['beverage'])
+    ep_record['people'] = agressive_splits(ep_record['people'])
+    ep_record['sources'] = agressive_splits(ep_record['sources'])
+    ep_record['themes'] = splits(ep_record['themes'])
+    ep_record['notable_bits'] = splits(ep_record['notable_bits'])
+    ep_record['mediawiki_description'] = pandoc.write(
+        pandoc.read(ep_record['details_html'], format="html-native_divs-native_spans"),
+        format="mediawiki"
+    )
 
-	parsed_description = wikitextparser.parse(ep_record['mediawiki_description'])
+    parsed_description = wikitextparser.parse(ep_record['mediawiki_description'])
 
-	# TODO(woursler): Remove citations external link, but add citations section?
-	if len(parsed_description.external_links) > 0:
-		print(ep_record['title'])
-		print(parsed_description.external_links)
+    # This is a bit hacky, but we need to pick up the correct citations from the external table.
+    ep_record['mediawiki_citations'] = None
+    if len(parsed_description.external_links) > 0:
+        for link in parsed_description.external_links:
+            link_url = link.url
+            if 'knowledgefight.com/research/' in link_url:
+                ep_record['mediawiki_citations'] = citations_df[citations_df.citations_url==link_url].citations_mediawiki.to_list()[0]
 
-	ep_record['release_date'] = maya.when(ep_record['release_date_x'], timezone=TIMEZONE).datetime().strftime(DATE_FORMAT)
+    ep_record['release_date'] = maya.when(ep_record['release_date_x'], timezone=TIMEZONE).datetime().strftime(DATE_FORMAT)
 
-	ep_record['coverage_start_date'] =  cleans(ep_record['coverage_start_date'])
-	if ep_record['coverage_start_date'] is not None:
-		ep_record['coverage_start_date'] = maya.when(ep_record['coverage_start_date'], timezone=TIMEZONE).datetime().strftime(DATE_FORMAT)
+    ep_record['coverage_start_date'] =  cleans(ep_record['coverage_start_date'])
+    if ep_record['coverage_start_date'] is not None:
+        ep_record['coverage_start_date'] = maya.when(ep_record['coverage_start_date'], timezone=TIMEZONE).datetime().strftime(DATE_FORMAT)
 
-	ep_record['coverage_end_date'] =  cleans(ep_record['coverage_end_date'])
-	if ep_record['coverage_end_date'] is not None:
-		ep_record['coverage_end_date'] = maya.when(ep_record['coverage_end_date'], timezone=TIMEZONE).datetime().strftime(DATE_FORMAT)
+    ep_record['coverage_end_date'] =  cleans(ep_record['coverage_end_date'])
+    if ep_record['coverage_end_date'] is not None:
+        ep_record['coverage_end_date'] = maya.when(ep_record['coverage_end_date'], timezone=TIMEZONE).datetime().strftime(DATE_FORMAT)
 
-	if ep_record['coverage_start_date'] == ep_record['coverage_end_date']:
-		ep_record['coverage_date'] = ep_record['coverage_start_date']
-	else:
-		ep_record['coverage_date'] = None
+    if ep_record['coverage_start_date'] == ep_record['coverage_end_date']:
+        ep_record['coverage_date'] = ep_record['coverage_start_date']
+    else:
+        ep_record['coverage_date'] = None
 
-	# Clean up obsolete fields
-	del ep_record['release_date_x']
-	del ep_record['release_date_y']
-	del ep_record['details_html']
-	del ep_record['description']
-	del ep_record['embed_player_url']
+    # Clean up obsolete fields
+    del ep_record['release_date_x']
+    del ep_record['release_date_y']
+    del ep_record['details_html']
+    del ep_record['description']
+    del ep_record['embed_player_url']
 
-	return ep_record
+    return ep_record
 
 def prettify_mediawiki(raw):
-	p1 = wikitextparser.parse(raw).pformat()
+    p1 = wikitextparser.parse(raw).pformat()
 
-	p2 = re.sub(r'(\n\s*)+\n+', '\n\n', p1)
+    p2 = re.sub(r'(\n\s*)+\n+', '\n\n', p1)
 
-	return p2
+    return p2
 
 
 RECORDS = merged_df.to_dict(orient='records')
 for raw_record in RECORDS:
-	record = process_ep_record(raw_record)
+    record = process_ep_record(raw_record)
 
-	raw = template.render(**record)
-	pretty = prettify_mediawiki(raw)
+    raw = template.render(**record)
+    pretty = prettify_mediawiki(raw)
 
-	#print("===== RAW ======\n\n\n\n")
-	#print(raw)
+    #print("===== RAW ======\n\n\n\n")
+    #print(raw)
 
-	#print("===== PRETTY ======\n\n\n\n")
-	#print(pretty)
+    #print("===== PRETTY ======\n\n\n\n")
+    #print(pretty)
 
-	#print("===== RECORD ======\n\n\n\n")
-	#pprint(record)
+    #print("===== RECORD ======\n\n\n\n")
+    #pprint(record)
 
-	slug = slugify(record['title'], separator='_')
+    slug = slugify(record['title'], separator='_')
 
-	with io.open('sample_pages/%s.wiki' % slug, mode="w", encoding="utf-8") as f:
-		f.write(pretty)
+    with io.open('sample_pages/%s.wiki' % slug, mode="w", encoding="utf-8") as f:
+        f.write(pretty)
 
 # STILL NEEDED
 # Automatic Links
