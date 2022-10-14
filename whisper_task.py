195c5c2bb7d477f281b9ec998946d3b10ff9c21e
Will Oursler <woursler@gmail.com>
2022-10-13 20:43:54 -0700

    Fixup transcripts and data

--- whisper_task.py
+++ whisper_task.py
@@ -4,8 +4,12 @@ from glob import glob
 import parse
 from os.path import exists
 
+i = 0
 for audio_fname in glob(r'audio_files\infowars\*.mp3'):
     episode_label = parse.parse(r"audio_files\infowars\{}.mp3", audio_fname)[0]
+
+    if not episode_label.startswith('2013'):
+        continue
 
     if exists(r'transcripts\infowars_tmp\%s.mp3.srt' % episode_label):
         print("Skipping", audio_fname)
@@ -14,3 +18,7 @@ for audio_fname in glob(r'audio_files\infowars\*.mp3'):
 
     print(
         r"whisper --model medium.en --language en --device cuda --output_dir=.\transcripts\infowars_tmp\ .\audio_files\infowars\%s.mp3 > .\transcripts\infowars_tmp\%s.txt" % (episode_label, episode_label))
+
+    i += 1
+    if i >= 4:
+        break
