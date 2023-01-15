7fe9015006c3cafba55c0b466e812f6959e020c8
Will Oursler <woursler@gmail.com>
2023-01-14 17:01:05 -0800

    Clean up how whisper_task works.

--- whisper_task.py
+++ whisper_task.py
@@ -1,35 +1,44 @@
 
 import autosub
-from glob import glob
+import click
 import parse
+
+from box import Box
+from glob import glob
 from os.path import exists
+from tqdm import tqdm
+from transcripts import create_best_transcript_listing
 
-commands = []
 
-N = 100
+@click.command()
+@click.option('--n_threads', default=2, help='Number of simultaneous whisper processes your GPU can run.')
+@click.option('--min-ep-num', default=0, help='Lowest numbered episode to include.')
+@click.option('--max-ep-num', default=10**3, help='Highest numbered episode to include.')
+def generate_whisper_commands(min_ep_num, max_ep_num, n_threads):
 
-i = 0
-#for audio_fname in glob(r'audio_files\infowars\*.mp3'):
-for episode_number in range(500, 600):
+    transcript_listing = create_best_transcript_listing()
+    bad_transcript_listing = transcript_listing[transcript_listing.transcript_type == 'autosub']
 
-    #episode_label = parse.parse(r"audio_files\infowars\{}.mp3", audio_fname)[0]
+    commands = []
+    for transcript_record in tqdm(bad_transcript_listing.to_dict(orient='records')):
+        transcript_record = Box(transcript_record)
 
-    #if not episode_label.startswith('2012'):
-    #    continue
+        ep_num = int(
+            ''.join([s for s in transcript_record.episode_number if s.isdigit()]))
 
-    #if exists(r'transcripts\infowars_tmp\%s.mp3.srt' % episode_label)  or exists(r'transcripts\infowars\%s.txt' % episode_label):
-    #    continue
+        if ep_num < min_ep_num:
+            continue
 
-    commands.append(
-#        r"whisper --model medium.en --language en --device cuda --output_dir=.\transcripts\infowars_tmp\ .\audio_files\infowars\%s.mp3" % episode_label)
-        r"whisper --model medium.en --language en --device cuda --output_dir=.\transcripts\ .\audio_files\%s.mp3" % str(episode_number))
+        if ep_num > max_ep_num:
+            continue
 
-    i += 1
-    if i >= 2*N:
-        break
+        commands.append(
+            r"whisper --model medium.en --language en --device cuda --output_dir=.\transcripts\ .\audio_files\%s.mp3" % transcript_record.episode_number)
 
-print('\n\n\n')
-print('; '.join(commands[0::2]))
-print('\n\n\n')
-print('; '.join(commands[1::2]))
-print('\n\n\n')
+    for i in range(n_threads):
+        print('\n\n\n')
+        print('; '.join(commands[i::n_threads]))
+
+
+if __name__ == '__main__':
+    generate_whisper_commands()
