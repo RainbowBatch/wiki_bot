312b8ac8bb933e329a4255542d76928d12f42768
Will Oursler <woursler@gmail.com>
2023-01-26 08:23:32 -0800

    Improve the export script, now that someone might be trying to revive the tracker.

--- final_export.py
+++ final_export.py
@@ -1,6 +1,24 @@
 import kfio
+import pandas as pd
 
 episodes_df = kfio.load('data/final.json')
 
+final_df = pd.DataFrame()
 
-episodes_df.to_csv('episodes.csv', index=False)
+final_df["Episode Number"] = episodes_df["episode_number"]
+final_df["Air Date"] = episodes_df["release_date"]
+final_df["Coverage Start Date"] = episodes_df["coverage_start_date"]
+final_df["Coverage End Date"] = episodes_df["coverage_end_date"]
+final_df["Type of Episode"] = episodes_df["episode_type"]
+final_df["Novelty Beverage"] = episodes_df["beverage"]
+final_df["Refereneced people/ Guests"] = episodes_df["people"].apply(lambda x: '; '.join(x))
+# Not reliably back-populated, so skipping.
+final_df["Books/ Primary sources"] = ''
+final_df["Themes"] = ''
+final_df["Out Of Context"] = ''
+final_df["Noteable Drops or Bits"] = ''
+final_df["Episode Description"] = episodes_df["mediawiki_description"].apply(lambda x: '  '.join(x.split('\n')).strip())
+
+print(final_df)
+
+final_df.to_csv('episodes.csv', index=False)
